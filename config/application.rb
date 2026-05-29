50a76bb1e230289a3a291e7bb0783e33ba0c40dc
Anthony Martin <amartin75@gmail.com>
2026-05-29 13:27:29 -0700

    Move framework defaults to Rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Hyperion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.active_storage.variant_processor = :disabled
 
